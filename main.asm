c562cfeba24e0b91f1e69e3f2c4cd0324de82f34
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2024-06-29 22:32:43 -0400

    Test program - Cycle bilge relay on and off every 30mS

--- main.asm
+++ main.asm
@@ -21,23 +21,74 @@
 ; *  
 ; ***
 
-; Includes
+; ** Includes **
 .include "tn13def.inc"
 
-; Reset vector
+; ** Definintions **
+.def TEMP = r17             ; Scratch Register
+
+.equ BILGE_RLY = PB0
+.equ LIGHT_RLY = PB1
+.equ PWR_LED   = PB2
+.equ BILGE_SW  = PB3
+.equ LIGHT_SW  = PB4
+
+; ** Reset vector **
 .org 0x0000
     rjmp RESET
 
 
-; Reset Sequence
+; ** Reset Sequence **
 RESET:
     ; Init Stack
     ldi r17, low(RAMEND)
     out SPL, r17
 
-; Main loop
+    ; Port setup
+    ; PB0 = BILGE_RLY
+    ; PB1 = LIGHT_RLY
+    ; PB2 = PWR_LED
+    ; PB3 = BILGE_SW
+    ; PB4 = LIGHT_SW
+    ; PB5 = ~RESET - Tied to +5V
+    
+    ldi TEMP, 0b00000111
+    out DDRB, TEMP
+    clr TEMP
+    out PORTB, TEMP
+
+
+
+
+; ** Main loop **
 MAIN:
-    nop
+    sbi PORTB, BILGE_RLY
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    cbi PORTB, BILGE_RLY
+rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
+    rcall DEBOUNCE
     rjmp MAIN
 
 
+DEBOUNCE:
+; Assembly code auto-generated
+; by utility from Bret Mulvey
+; Delay 6 000 cycles
+; 5ms at 1.2 MHz
+    ldi  r18, 8
+    ldi  r19, 202
+L1: dec  r19
+    brne L1
+    dec  r18
+    brne L1
+    nop
+    RET
